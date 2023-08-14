1baf37117af4980da2e1036b73349dfe2375b8e3
nikhil <nikhilnsr1998@gmail.com>
2023-08-14 17:59:17 +0530

    sorted the installations

--- ubuntu-post-install-nikhil.sh
+++ ubuntu-post-install-nikhil.sh
@@ -34,40 +34,6 @@ sudo apt update
 sudo apt install code
 
 
-#DOCKER installation
-for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done	#uninstalled old docker installation
-
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo docker run hello-world
-sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker
-docker run hello-world
-# to stop docker daemon rather than just a container,
-# sudo systemctl stop docker
-# to restart docker daemon
-# sudo systemctl start docker
-
-#sudo apt install docker-compose -y for install version 1 of compose and below steps for version 2
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-
-curl -SL https://github.com/docker/compose/releases/download/v2.6.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
-# now you may only need to add the alias for $DOCKER_CONFIG/cli-plugins/docker-compose into the zshrc file.
-
-
 #CHROME installation
 sudo apt install wget
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -96,19 +62,16 @@ gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 32
 sudo apt-get install openssh-server
 sudo systemctl status ssh
 sudo systemctl start ssh
-sudo systemctl stop ssh
 #set the ssh key for github access.
 ssh-keygen -o -t rsa -C "nikhilnsr1998@gmail.com"
-cat .ssh/id_rsa.pub
 # copy the contents of the public key and add to github keys
 read -p "Press any key to resume ..."
 
-#Torrent client
+#File Sharing
 sudo apt update
 sudo apt upgrade
 sudo apt install qbittorrent -y
 sudo apt install onedrive -y
-
 
 #ZSH
 sudo apt-get update
@@ -116,5 +79,47 @@ sudo apt-get install zsh -y
 chsh -s $(which zsh)
 echo '''you need to retart now and then run below command
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-you might get a connection failure with guthub in that case you need to add it to the file /etc/hosts 
+you might get a connection failure with github in that case you need to append the following to the file /etc/hosts 
+
+echo "185.199.108.133 raw.githubusercontent.com" >> /etc/hosts
+
+also copy the contents of the public key within ~/.ssh and add over to github
 '''
+sudo -i
+
+#DOCKER installation
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done	#uninstalled old docker installation
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker run hello-world
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+docker run hello-world
+# to stop docker daemon rather than just a container,
+# sudo systemctl stop docker
+# to restart docker daemon
+# sudo systemctl start docker
+
+#sudo apt install docker-compose -y
+#for version 1 of compose and below steps
+
+#for version 2
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+
+curl -SL https://github.com/docker/compose/releases/download/v2.6.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+# now you may only need to add the alias for $DOCKER_CONFIG/cli-plugins/docker-compose into the zshrc file.
+
